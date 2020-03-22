a76a22c4e77766d90dd6a9d96b87815c17a7f784
Robert Rydlewski <robertrydlewski1984@gmail.com>
2020-03-22 20:14:57 +0100

    methods in objects

--- JS/app.js
+++ JS/app.js
@@ -12,11 +12,17 @@ resetPlayer: function(classType) {
             player = new Player(classType, 100, 0, 100, 150, 200);
             break;
         case "Mage":
-            player = new Player(classType, 80, 0, 50, 200, 100);
-        break;
+            player = new Player(classType, 80, 0, 50, 200, 500);
+            break;
+        case "Hunter":
+                player = new Player(classType, 200, 0, 50, 200, 100);
+            break;
     default:
     }
+    let getInterface = document.querySelector(".interface");
+    getInterface.innerHTML = '<img src="img/characters/' + classType.toLowerCase() + '.png" class="img-avatar"><div><h3>' + classType + '</h3><p>Health: ' + player.health + '</p><p>Mana: ' + player.mana + '</p><p>Strength: ' + player.strength + '</p><p>Agility: ' + player.agility + '</p><p>Speed: ' + player.speed + '</p></div>';
 },
+
 setPreFight: function() {
 
 }
