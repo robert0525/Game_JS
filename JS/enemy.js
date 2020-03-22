352b164ed16bd566152b2396d04bdbf1e2e2faa8
Robert Rydlewski <robertrydlewski1984@gmail.com>
2020-03-22 17:14:26 +0100

    secound commit

---
+++ JS/enemy.js
@@ -0,0 +1,8 @@
+function Enemy (ClassType, health, mana, strength, agility, speed ) {
+    this.ClassType = ClassType;
+    this.health = health;
+    this.mana = mana;
+    this.strength = strength;
+    this.agility = agility;
+    this.speed = speed;
+}
--- JS/player.js
+++ JS/player.js
@@ -1,3 +1,8 @@
-function Player () {
-    
+function Player (ClassType, health, mana, strength, agility, speed ) {
+    this.ClassType = ClassType;
+    this.health = health;
+    this.mana = mana;
+    this.strength = strength;
+    this.agility = agility;
+    this.speed = speed;
 }
