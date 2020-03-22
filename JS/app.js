cbb1ca28d65b04b23f7b91bf7f095eb47e6e5272
Robert Rydlewski <robertrydlewski1984@gmail.com>
2020-03-22 19:37:51 +0100

    javascrit update

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,24 @@
 let GameManger = {
-]
-    setGameStart: function() {
-
-        
+setGameStart: function(classType) {
+    this.resetPlayer(classType);
+    this.setPreFight();
+    },
+resetPlayer: function(classType) {
+    switch( classType) {
+        case "Warrior":
+            player = new Player(classType, 200, 0, 200, 100, 50);
+            break;
+        case "Rogue":
+            player = new Player(classType, 100, 0, 100, 150, 200);
+            break;
+        case "Mage":
+            player = new Player(classType, 80, 0, 50, 200, 100);
+        break;
+    default:
     }
+},
+setPreFight: function() {
+
+}
+
 }
